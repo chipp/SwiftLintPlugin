7202dff0196171d7baccd0ebd7b1ea5052c4713b
Vladimir Burdukov <chippcheg@gmail.com>
2026-01-13 11:58:52 +0200

    Use custom verb for SwiftLintRun

--- Package.swift
+++ Package.swift
@@ -26,9 +26,12 @@ let package = Package(
         ),
         .plugin(
             name: "SwiftLintRun",
-            capability: .command(intent: .sourceCodeFormatting(), permissions: [
-                .writeToPackageDirectory(reason: "Fixing SwiftLint issues")
-            ]),
+            capability: .command(
+                intent: .custom(verb: "swiftlint", description: "Run swiftlint"),
+                permissions: [
+                    .writeToPackageDirectory(reason: "Fixing SwiftLint issues")
+                ]
+            ),
             dependencies: [
                 .target(name: "SwiftLintBinary")
             ]
